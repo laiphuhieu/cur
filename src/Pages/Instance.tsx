26dbd8b3562ef76dda493afeef2878c25ee30b63
laiphuhieu <laiphuhieu123@gmail.com>
2023-06-08 00:55:06 +0700

    Revert "add/remove item of array"
    This reverts commit cada1742fea9fef630336b91606fb1255c08f471.

--- src/Pages/Instance.tsx
+++ src/Pages/Instance.tsx
@@ -12,11 +12,11 @@ const temp = {
 };
 
 const Instance = () => {
-  const [instances, setInstances] = useState<Item[]>([]);
+  const [instance, setInstance] = useState<Item[]>([]);
 
   const getItem = useCallback(() => {
     axios.get("https://pokeapi.co/api/v2/item/").then((response) => {
-      setInstances(response.data.results);
+      setInstance(response.data.results);
     });
   }, []);
 
@@ -24,38 +24,15 @@ const Instance = () => {
     getItem();
   }, [getItem]);
 
-  const handleClickAdd = useCallback(() => {
-    setInstances((prev) => [...prev, temp]);
-  }, []);
-
-  const handleClickRemove = useCallback(() => {
-    setInstances((prev) => prev.slice(0, prev.length - 1));
-  }, []);
-
   return (
     <div>
-      <button
-        onClick={handleClickAdd}
-        style={{ margin: "24px", padding: "36px", background: "#fff" }}
-      >
-        Push to state array
-      </button>
-      <button
-        onClick={handleClickRemove}
-        style={{ margin: "24px", padding: "36px", background: "#fff" }}
-      >
-        Remove last item from array
-      </button>
-      <div>
-        {instances.map((result: Item, id: number) => {
-          console.log(result.name);
-          return (
-            <ul key={id}>
-              <li>{result.name}</li>
-            </ul>
-          );
-        })}
-      </div>
+      {instance.map((result: Item, id: number) => {
+        return (
+          <ul key={id}>
+            <li>{result.name}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
